7091094185aad04091d70a4a16c6a1e0e52dcb37
gergokutu <gergo.kutu@gmail.com>
2019-07-05 16:28:23 +0200

    next question

--- index.js
+++ index.js
@@ -14,30 +14,53 @@ const flashCard = [
 ]
 
 // get random item from array
-const randomCard = flashCard[Math.floor(Math.random() * flashCard.length)]
-const randomQuestion = randomCard.question
-const randomAnswer = randomCard.answer
+// let randomNumber = Math.floor(Math.random() * ((flashCard.length - 1) + 1))
+// let randomCard = flashCard[randomNumber]
 
-const question = document.createElement('p')
-question.innerHTML = randomQuestion
-
-const sectionQuestion = document.getElementById('question')
-sectionQuestion.appendChild(question)
+const questionArray = []
+const answerArray = []
 
 const button = document.createElement('button')
 
-function clickStopper(e) {
-    alert("click was blocked"); // ** you can remove this line **
-    e.preventDefault(); // equivalent to 'return false'
+function showQuestion() {
+    // empty the question and the answer
+    document.getElementById('question').innerHTML = null
+    document.getElementById('answer').innerHTML = null
 
+    const randomNumber = Math.floor(Math.random() * ((flashCard.length - 1) + 1))
+    const randomCard = flashCard[randomNumber]
+    const randomQuestion = randomCard.question
+    const question = document.createElement('p')
+    question.innerHTML = randomQuestion
+
+    const sectionQuestion = document.getElementById('question')
+    sectionQuestion.appendChild(question)
+    questionArray.push(randomQuestion)
 }
+
+showQuestion()
 
 function showAnswer() {
     const answer = document.createElement('p')
-    answer.innerHTML = randomAnswer
-
     const sectionAnswer = document.getElementById('answer')
-    sectionAnswer.appendChild(answer)
-
+    for (let i = 0; i < flashCard.length; i++) {
+        if (document.getElementById('question') === flashCard[i].question) {
+            document.getElementById('answer').innerHTML = flashCard[i].answer
+            console.log(flashCard[i].answer)
+            sectionAnswer.appendChild(answer)
+        }
+    }
 }
 
+// function showAnswer() {
+//     const randomAnswer = randomCard.answer
+//     answerArray.push(randomAnswer)
+//     // prevent the click button to show the answer multiple times
+//     document.getElementById('answer').innerHTML = null
+
+//     const answer = document.createElement('p')
+//     answer.innerHTML = randomAnswer
+
+//     const sectionAnswer = document.getElementById('answer')
+//     sectionAnswer.appendChild(answer)
+// }
