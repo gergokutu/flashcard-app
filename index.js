3a9112dcb20804787e0ea39742d331682fe09f58
gergokutu <gergo.kutu@gmail.com>
2020-02-04 17:26:11 +0100

    Cleaning

--- index.js
+++ index.js
@@ -35,10 +35,8 @@ const flashCards = {
                 answer: 'Frog kick'
             }
         ]
-
 }
 
-// choose category
 function categorySelect() {
     const categories = document.getElementById('categorySelect').children
     for (let i = 0; i < categories.length; i++) {
@@ -48,7 +46,7 @@ function categorySelect() {
 
 function showQuestion() {
     const category = categorySelect()
-    // empty the question and the answer
+    
     document.getElementById('question').innerText = null
     document.getElementById('answer').innerText = null
 
@@ -66,8 +64,7 @@ function showAnswer() {
 
     flashCards[category].map((item) => {
         if (item.question === question.innerText) answerSection.innerText = item.answer
-        // advanced console methods > count
-        // console.count('Console.count')
+        
     })
 }
 
@@ -78,7 +75,7 @@ function showFeedback(feedback, message) {
 
 function addOwnCard() {
     const category = categorySelect()
-    console.log(category)
+
     flashCards[category].push(
         {
             id: flashCards[category].length,
@@ -91,7 +88,6 @@ function addOwnCard() {
     feedback.innerText = "Q&A created"
     setTimeout(() => feedback.innerText = "", 4000)
 
-    // advanced console methods
     console.log(
         '%cNewly created flashCard:', 'color: blue; font-size: small;',
         flashCards[category][flashCards[category].length - 1]
@@ -145,79 +141,3 @@ function addCategory() {
 }
 
 showQuestion()
-
-// // advanced console methods > color, font-size
-// console.log('%cConsole.log message with x-large orange letters', 'color: orange; font-size: x-large;')
-
-// // console.dir
-// const myObject = {
-//     first: {
-//         name: "Pocok",
-//         age: 999
-//     },
-//     second: {
-//         name: "Kutu",
-//         age: 000
-//     }
-// }
-
-// console.log('console.log:', myObject)
-// console.dir(myObject)
-
-// // console.table
-// console.table(flashCards.coding)
-// console.table(myObject)
-
-// // console.group, ...End(), ...Collapsed
-// // identation just for better readability
-// // console.group('%cMain group:', 'color: red; font-size: large; font-weight: bold;')
-// console.groupCollapsed('%cMain group:', 'color: red; font-size: large; font-weight: bold;')
-//     console.group('%cSubgroup 1:', 'color: orange;')
-//         console.log('item1')
-//         console.log('item1')
-//         console.log('item1')
-//         console.groupEnd()
-//     console.groupCollapsed('%cSubgroup 2:', 'color: blue;')
-//         console.log('item2')
-//         console.log('item2')
-//         console.log('item2')
-//         console.groupEnd()
-// console.groupEnd()
-
-// // console the message in particular cases > assert()
-// const errorMessage = 'Hey the number is not even'
-// for (let i = 0; i < 10; i++) {
-//     console.assert(i % 2 === 0, { i: i, errorMessage: errorMessage })
-// }
-
-// // console.trace()
-// // This method displays a trace that shows how the code ended up at a certain point.
-// function myFunction() {
-//     myOtherFunction()
-// }
-
-// function myOtherFunction() {
-//     console.trace()
-// }
-
-// myOtherFunction()
-// myFunction()
-
-// // console.time(), ...console.timeEnd()
-// // tracking the time for actions
-// // console.time() is a better way to track the microtime taken for JavaScript executions
-// console.time('Action took:')
-// let total = 0
-// for (i = 0; i < 10000; i++) {
-//     total += i
-// }
-// console.log('Total value:', total)
-// console.timeEnd('Action took:')
-
-// // console.memory()
-// // how our JavaScript applications are using browser memory
-// console.log(console.memory)
-// // or simply write it into the console > console.memory
-
-// // destroy all console messages
-// // console.clear()
